0f65d8f145a69ab7bbdd90d21b6608bc00c7cbec
Guilherme B <guih.smi2@outlook.com>
2019-08-27 13:54:42 +0000

    Inclusao da Requisicao dinamica

---
+++ SpringMVC/src/main/java/com/desenvolvimento/controller/OlaMundoController.java
@@ -0,0 +1,49 @@
+package com.desenvolvimento.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.*;
+
+/* Area de Rotas do SpringMVC */
+
+@Controller
+public class OlaMundoController {
+   @RequestMapping("/olaMundoSpring")
+   public String execute(){
+       System.out.println("Executando à lógica com Spring MVC");
+       return "ok"; 
+   }
+
+   @RequestMapping("/teste")
+   public String executa(){
+       System.out.println("Execucao de teste apenas para ver");
+       return "teste"; 
+   }
+  /* Paginação de erro customizada */
+   @RequestMapping(value="errors",method = RequestMethod.GET)
+   public ModelAndView renderErrorPage(HttpServletRequest httpRequest){
+       System.out.println("Entrou no Erro");
+       ModelAndView modelo = new ModelAndView("errorPage");
+       return modelo;
+   } 
+   /* Rota para a pagina inicial*/
+   @RequestMapping(value="/",method=RequestMethod.GET)
+   public String home(Locale locale, Model model){
+       System.out.println("Pagina inicial requisitada!");
+       Date dataLocal = new Date();
+       DateFormat dataFormatada = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+       String dataFinal = dataFormatada.format(dataLocal);
+       System.out.println("Data = " + dataFinal); 
+      // Passa o Valor para a pagina requsitada 
+       model.addAttribute("serverTime",dataFinal);
+       return "home";
+   }  
+}
---
+++ SpringMVC/src/main/webapp/WEB-INF/views/ok.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<h1>Jamal Spring MVC - Conseguiu!</h1>
+</body>
+</html>
