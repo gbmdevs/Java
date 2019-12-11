d4f77c3cf3ebc229ef2f51eb88bb17d078f682c6
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2019-12-11 13:01:20 +0000

    Inserindo DB

--- SisteFinanPes/Planilha/src/main/java/Financeiro/Controller/MainController.java
+++ SisteFinanPes/Planilha/src/main/java/Financeiro/Controller/MainController.java
@@ -6,12 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+// Postgres
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 @Controller
 public class MainController{
-   
+    // Variaveis do DB
+     String driver = "org.postgresql.Driver";
+     String user   = "postgres";
+     String pass   = "";
+     String url    = "jdbc:postgresql://localhost:5432/postgres"; 
+     Connection con = null;    
+
 @RequestMapping(value = "/", method = RequestMethod.GET)
   public String main(){
-      System.out.println("Bora Galera!");
+       
+       try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,user,pass);
+            System.out.println("Banco de Dados Conectado");
+       }catch(Exception e){
+            System.out.println(e);
+       }      
+
       return "index";
   }
 
