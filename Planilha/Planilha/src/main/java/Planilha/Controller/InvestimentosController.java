a42cc6d4a35ba3f9baececd9c5bed4681101a422
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2020-04-17 19:49:39 +0000

    Att JBOSS

---
+++ Planilha/Planilha/src/main/java/Planilha/Controller/InvestimentosController.java
@@ -0,0 +1,15 @@
+package Planilha.Controller.InvestimentosController;
+
+import java.util.ArrayList;
+
+public class InvestimentosController{
+    
+     private List<Investimentos> listinvest;
+
+
+
+}
+
+
+
+
---
+++ Planilha/Planilha/src/main/java/Planilha/Model/Investimentos.java
@@ -0,0 +1,21 @@
+package Planilha.Model.Investimentos;
+
+public class Investimentos{
+       
+      private  Integer idinvest;  
+      private  Integer idtipinvest;
+      private  Double  valueinvest;
+
+public Investimentos(){
+              
+}
+
+public void setIdInvest(Integer idinvest){
+     this.idinvest = idinvest;
+}
+
+public Integer getIdInvest(){
+    return this.idinvest;
+}
+
+}
