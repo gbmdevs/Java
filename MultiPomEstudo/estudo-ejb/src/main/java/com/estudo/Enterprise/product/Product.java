e11c05e401691aa55ebf3c5a41915dcb64e113b2
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2021-07-07 18:29:37 +0000

    Inicio estudos da migracao

---
+++ MultiPomEstudo/estudo-ejb/src/main/java/com/estudo/Enterprise/product/Product.java
@@ -0,0 +1,7 @@
+package com.estudo.Enterprise.product;
+
+import java.io.Serializable;
+
+public class Product implements Serializable { 
+    private static final long serialVersionUID = 1L;
+}
---
+++ MultiPomEstudo/estudo-web/src/main/java/com/estudo/Enterprise/servlet/ShoppingCartServlet.java
@@ -0,0 +1,17 @@
+package com.estudo.Enterprise.servlet;
+
+import java.io.IOException;
+ 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ShoppingCartServlet")
+public class ShoppingCartServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+}
