7763e7b6f19f4f3677e263a3f1ccfecd91efcf27
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2022-05-13 03:34:05 +0000

    Atualizacao da planilinha

--- PlanilhaV2/planilha-banco/src/main/java/br/banco/util/DaoGenerico.java
+++ PlanilhaV2/planilha-banco/src/main/java/br/banco/util/DaoGenerico.java
@@ -3,12 +3,19 @@ package br.banco.util;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
+
+import javax.inject.Inject;
 
 public class DaoGenerico<T extends EntidadeBase> {
 	
+
+	@PersistenceContext
 	private static EntityManager manager = ConnectionFactory.getEntityManager();
 
 	public T findById(Class<T> clazz, Long id){
+		System.out.println("Etrnou no find");
 		return manager.find(clazz, id);
 	}
 
--- PlanilhaV2/planilha-rs/src/main/java/br/com/requisicao/requisicoes/DetalhaTipoEstrategia.java
+++ PlanilhaV2/planilha-rs/src/main/java/br/com/requisicao/requisicoes/DetalhaTipoEstrategia.java
@@ -5,22 +5,30 @@ import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 
+import br.banco.entidade.TipoEstrategia;
+
 import br.com.requisicao.dto.Estudante;
 
 import br.com.requisicao.util.ConverteJSON;
 
+import javax.inject.Inject;
+
+import  br.banco.util.DaoGenerico;
 
 @Path("/tipoestrategia")
 public class DetalhaTipoEstrategia extends ConverteJSON{
  
+     
+
      @GET
      public String buscarTiposEstrategias(){
-           Estudante estudante = new Estudante();
-           estudante.setNome("Jamal");
+           TipoEstrategia estudante = new TipoEstrategia();
 
            // Buscar todos os dados do tipo Estrategia
+           DaoGenerico<TipoEstrategia> dao = new DaoGenerico<TipoEstrategia>();
+           estudante = dao.findById(TipoEstrategia.class, 1L);
 
-
+           System.out.println("Passou do find");
 
            return new ConverteJSON().retornaJSON(estudante);
 
