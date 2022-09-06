8943e1c401467777edaec22947ac2d820e25e1f7
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2022-09-06 02:41:50 +0000

    Gravacao de csv via get

--- angular13-springboot/backend/src/main/java/br/com/springboot/controller/MainController.java
+++ angular13-springboot/backend/src/main/java/br/com/springboot/controller/MainController.java
@@ -7,9 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 
 import org.springframework.beans.factory.annotation.Autowired;  
 
@@ -25,14 +32,47 @@ import org.springframework.batch.core.Job;
 @RestController
 @RequestMapping("/api")
 public class MainController{
-    // @Autowired
-    // JobLauncher jobLauncher;
+     @Autowired
+     JobLauncher jobLauncher;
  
-    // @Autowired
-    // Job processJob;
+     //@Autowired
+     //Job processJob;
      
      @Autowired
      EstudanteService estudanteService;
+
+     // Area de Transferencia de Arquivos
+     @PostMapping("/cargastock")
+     public String carregaArquivosStock(@RequestParam("files") MultipartFile[] files){
+        System.out.println("Tamanho dos Arquivos: " + files.length);
+        System.out.println("Entrou na Carga das Stocks");
+        
+        String arquivo = "arquivo_base";
+        int contador;
+        
+        try{
+        // Caminho absoluto dos arquivos
+            for(contador = 0;contador<files.length;contador++){
+                byte[] bytes = files[contador].getBytes();
+                String pathRoot = new File("").getAbsolutePath();
+                System.out.println("Path:" + pathRoot );
+                File dir = new File(pathRoot + File.separator + "tmpFiles");
+                if(!dir.exists()){
+                    dir.mkdirs();
+                }
+                File serverFile = new File(dir.getAbsolutePath() + File.separator + arquivo + "_" + contador+ ".csv" );
+                BufferedOutputStream stream =  new BufferedOutputStream(new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+            }
+
+            return  "Arquivo subiu com sucesso! - Total de Arquivos: " + contador;
+        }catch(Exception ex){
+           ex.printStackTrace();
+           return "Falha ocorrida no upload do arquivo. Arquivo vazio.";
+        }       
+
+     }
 
      //List
      @GetMapping("/estudantes")
