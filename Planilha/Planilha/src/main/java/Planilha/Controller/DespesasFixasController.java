6d4966d105f3403760adb31a0f611f70bd2fe73f
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2023-08-30 04:00:12 +0000

    Att planilha

--- Planilha/Planilha/src/main/java/Planilha/Controller/DespesasFixasController.java
+++ Planilha/Planilha/src/main/java/Planilha/Controller/DespesasFixasController.java
@@ -21,8 +21,8 @@ import Planilha.Controller.DatasController;
 
 public class DespesasFixasController {
        private List<DespesasFixas>  despesasFixas = new ArrayList<DespesasFixas>();
-       private static String DATA_INICIO = "'2023-08-01'";
-       private static String DATA_FINAL  = "'2023-09-01'";
+       private static String DATA_INICIO = "'2023-09-01'";
+       private static String DATA_FINAL  = "'2023-10-01'";
   
 public void setDespesasFixas(List<DespesasFixas> despesasFixas){
     this.despesasFixas = despesasFixas;
