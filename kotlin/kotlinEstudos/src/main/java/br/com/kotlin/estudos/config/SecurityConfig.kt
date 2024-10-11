f8f6823693a7b232b09688986954a261ecfc6ee7
Guilherme Gois Braga de Medeiros <guilherme.gois@tqi.com.br>
2024-10-11 15:16:59 -0300

    Ajuste projeto kotlin

--- kotlin/kotlinEstudos/src/main/java/br/com/kotlin/estudos/config/SecurityConfig.kt
+++ kotlin/kotlinEstudos/src/main/java/br/com/kotlin/estudos/config/SecurityConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import br.com.kotlin.estudos.filter.JwtAuthenticationFilter
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -12,22 +13,24 @@ import org.springframework.security.config.http.SessionCreationPolicy
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(private val jwtAuthenticationFilter: JwtAuthenticationFilter){
+class SecurityConfig(private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+                     private val authenticationProvider: AuthenticationProvider){
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain{
         http.csrf().disable()
             .cors().disable()
             .headers().frameOptions().disable()
-            .and() 
+            .and()
             .authorizeHttpRequests()
             .requestMatchers(AntPathRequestMatcher("/auth/**")).permitAll()
-            .requestMatchers(AntPathRequestMatcher("/h2-console/**")).permitAll()           
+            .requestMatchers(AntPathRequestMatcher("/h2-console/**")).permitAll()
             .anyRequest().authenticated()
             .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
+            .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
     }
--- kotlin/kotlinEstudos/src/main/resources/db/migration/V1_999__MassaDeTeste.sql
+++ kotlin/kotlinEstudos/src/main/resources/db/migration/V1_999__MassaDeTeste.sql
@@ -1,8 +1,17 @@
 insert into usuario values('0a232a0e-82e8-4d6a-bd3b-f629fc2e8f80','guih.smi2@gmail.com','$2a$10$mv3sE8fvLKRR85AoC1NPN.sPsFaDYrSPKijFaaxVr1lsVnYZLRi1W',NULL,'Guilherme Gois Braga de Medeiros');
 
---insert into tip_desp_fixa values('ee122e7d-e1e8-4ded-822b-2ea4accbe092','0a232a0e-82e8-4d6a-bd3b-f629fc2e8f80',1,'Fatura Claro');
+insert into tip_desp_fixa values('ee122e7d-e1e8-4ded-822b-2ea4accbe092','0a232a0e-82e8-4d6a-bd3b-f629fc2e8f80',1,'Fatura Claro');
 --insert into tip_desp_fixa values('02643520-60db-4cb9-9c60-864e31ba26ce','0a232a0e-82e8-4d6a-bd3b-f629fc2e8f80',2,'Nubank - Cartão');
+
+insert into type_balance values(1,'Iti Itaú','Conta corrente do iti itau.',true);
+insert into type_balance values(2,'Banco Inter','Conta corrente do Inter.',true);
+
+insert into balance values('4545e64c-446d-4d2a-ba9e-711fde482995','0a232a0e-82e8-4d6a-bd3b-f629fc2e8f80',1,-24.59,CURRENT_TIMESTAMP);
+insert into balance values('ae7cf29e-744a-4979-8e5d-449dedad0ba8','0a232a0e-82e8-4d6a-bd3b-f629fc2e8f80',1,57.23,CURRENT_TIMESTAMP);
+
 
 insert into stock values('e4557f86-5770-45f5-ac90-327c2e48e1b4','VALE3','Vale Brasil','SA',true);
 insert into stock values('6d4a2098-2dcb-474a-a262-afacd99fb67e','BBAS3','Banco do Brasil','SA',true);
 insert into stock values('6583fac0-732c-4d2d-940d-8cf851709175','HASH11','Banco do Brasil','SA',true);
+insert into stock values('2ac0bfe8-68bc-45a0-80ba-ef485dc71eb3','MXRF11','Fundo IFIX','SA',true);
+insert into stock values('8fa10054-1e6c-4378-b0b9-11f274262917','USIM5','Fundo IFIX','SA',true);
